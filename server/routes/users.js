4ee2e053bec21186a556bf3ef05def60a4e9cf1f
T-ferdi <tristan.f5.130@gmail.com>
2024-02-15 11:34:49 -0800

    Added routes for deleting and updating users

--- server/routes/users.js
+++ server/routes/users.js
@@ -118,5 +118,44 @@ router.put('/user/update/:id', async(req, res)=>{
   }
 });
 
+//Delete an user
+router.delete('/user/:id', async(req, res)=>{
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if(!deletedUser){
+      return res.status(404).send({message: 'User not found'});//Error handling
+    }
+    res.json({ message: 'User deleted successfully', deletedUser });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({message: 'Internal server error'});//Error handling
+  }
+});
+
+//Update an user with new username, email, or password
+router.put('updateUser/:id', async(req, res)=>{
+  try {
+    const userId = req.params.id;
+    const updateFields = req.body; //Array of specific field(s) in user
+
+    let user = await User.findById(userId);
+
+    if(!updatedUser){
+      return res.status(404).send({message: 'User not found'});//Error handling
+  }
+    for (let field in updateFields){
+      user[field] = updateFields[field]; //Update specific field
+    }
+
+    user = await user.save() //Save updated user in database
+    res.json({ message: 'User updated successfully', updatedUser });
+
+  } catch (error) {//Error handling
+    console.log(error)
+    res.status(500).send({message: 'Internal server error'});
+  }
+});
+
 module.exports = router;
 
