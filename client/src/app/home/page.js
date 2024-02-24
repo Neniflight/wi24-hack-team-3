2f4caae5012de364ce81722431d84a499206e1c0
Murou Wang <muwang@ucsd.edu>
2024-02-23 20:30:20 -0800

    Creation of basic nav bar

---
+++ client/src/app/home/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './style.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main} >
+      <h1>Welcome to moo</h1>
+    </main>
+  )
+}
