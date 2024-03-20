86dd6a6995f766854ac7f82d80b50b0b4b7d5985
victoriatr6n <108353792+victoriatr6n@users.noreply.github.com>
2024-03-19 22:27:50 -0700

    pop up w/o overlay

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,14 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
 import React from 'react';
 import logo from 'public/images/logo.png';
 import Button from '@/components/button-component/button';
 import buttonStyle from '@/components/button-component/button.module.css';
+import popUpStyle from '@/components/popup-component/popup.module.css';
 import Divider from '@/components/divider-component/divider';
+import Popup from 'reactjs-popup';
+import { useState } from "react";
 
 export default function Home() {
   return (
@@ -13,11 +17,57 @@ export default function Home() {
             <h1 className={styles.welcome}>welcome to <span style={{ color: '#2B7E59' }}>cultivate!</span></h1>
             <p className={styles.message}><em>cultivate</em> is designed to help make your happiness a habit. 
             write down a happy memory and grow your garden!</p>
-            <div className={styles.buttonContainer}>
+            {/* <div className={styles.buttonContainer}>
                 <Button className={buttonStyle.signUp}>Sign Up</Button>
                 <Divider text="already have an account?"/>
                 <Button className={buttonStyle.logIn}>Log In</Button>
-            </div> 
+            </div>  */}
+            <br></br>
+            <div>
+            <Popup trigger=
+                {<button className={popUpStyle.signup}> Sign up </button>} 
+                modal nested>
+                {
+                    close => (
+                        // <div className={popUpStyle.overlay}>
+                        <div className={popUpStyle.modal}>
+                            <div className={popUpStyle.sign}>
+                                Sign up for an account
+                            </div>
+                            <div>
+                                <button className={popUpStyle.createAcc} onClick=
+                                    {() => close()}>
+                                        create account
+                                </button>
+                            </div>
+                        </div>
+                        // </div>
+                    )
+                }
+            </Popup>
+        </div>
+        <Divider text="already have an account?"/>
+        <div>
+            <Popup trigger=
+                {<button className={popUpStyle.logIn}> Log In </button>} 
+                modal nested>
+                {
+                    close => (
+                        <div className={popUpStyle.modal}>
+                            <div className={popUpStyle.sign}>
+                                Log into account
+                            </div>
+                            <div>
+                                <button className={popUpStyle.logInAcc} onClick=
+                                    {() => close()}>
+                                        log in
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+        </div>
 
         </div>
         <div className={styles.imageContainer}>
