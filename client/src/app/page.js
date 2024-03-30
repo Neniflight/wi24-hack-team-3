185befc3ca3985a41df2fa5d112d0d1aa4ebd92b
Leo Weng <leoweng05@gmail.com>
2024-03-30 10:22:41 -0700

    Reset to branch before Vicky's commits

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -13,74 +13,24 @@ import { useState } from "react";
 export default function Home() {
   return (
     <main className={styles.main}>
-        <div className={styles.leftColumn}>
+        <div className={styles.landing}>
             <h1 className={styles.welcome}>welcome to <span style={{ color: '#2B7E59' }}>cultivate!</span></h1>
             <p className={styles.message}><em>cultivate</em> is designed to help make your happiness a habit. 
             write down a happy memory and grow your garden!</p>
-            {/* <div className={styles.buttonContainer}>
+            <div className={styles.buttonContainer}>
                 <Button className={buttonStyle.signUp}>Sign Up</Button>
                 <Divider text="already have an account?"/>
                 <Button className={buttonStyle.logIn}>Log In</Button>
-            </div>  */}
-            <br></br>
-        <div className={popUpStyle.flexContainer}>
-                <Popup trigger=
-                    {<button className={popUpStyle.signup}> Sign up </button>} 
-                    modal nested>
-                    {
-                        close => (
-                            <div className={popUpStyle.modal}>
-                                <Image src={logo} className={popUpStyle.logo}></Image>
-                                <div className={popUpStyle.sign}>
-                                
-                                    create an account
-                                </div>
-                                <div>
-                                    <button className={popUpStyle.createAcc} onClick=
-                                        {() => close()}>
-                                            create account
-                                    </button>
-                                </div>
-                            </div>
-                        )
-                    }
-                </Popup>
+            </div>
         </div>
-        <Divider text="already have an account?"/>
-        <div className={popUpStyle.flexContainer}>
-            
-                <Popup trigger=
-                    {<button className={popUpStyle.logIn}> Log In </button>} 
-                    modal nested>
-                    {
-                        close => (
-                            <div className={popUpStyle.modal}>
-                                <Image src={logo} className={popUpStyle.logo}></Image>
-                                <div className={popUpStyle.sign}>
-                                    
-                                    Log into account
-                                </div>
-                                <div>
-                                    <button className={popUpStyle.logInAcc} onClick=
-                                        {() => close()}>
-                                            log in
-                                    </button>
-                                </div>
-                            </div>
-                        )
-                    }
-                </Popup>
-           
-        </div>
-
-        </div>
-        <div className={styles.rightColumn}>
+        <div className={styles.imageContainer}>
             <Image src={logo} className={styles.logo}></Image>
             <div className={styles.quoteContainer}>
                 <p><strong>"cultivate your garden"</strong></p>
                 <p className={styles.author}>- Voltaire, Candide</p>
             </div>
         </div>
+        
     </main>
   )
 }
