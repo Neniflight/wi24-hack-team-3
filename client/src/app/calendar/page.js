6a8d3ecbb6db8e9a4cb2db60f77ddd70cb05f60b
Murou Wang <muwang@ucsd.edu>
2024-04-04 17:55:21 -0700

    Janky Calendar

--- client/src/app/calendar/page.js
+++ client/src/app/calendar/page.js
@@ -1,11 +1,25 @@
+"use client";
+
 import Image from 'next/image'
-import styles from './style.module.css'
 import Layout from "../layout.js"
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
+import styles from './style.module.css'
 
 export default function Home() {
+  const [value, onChange] = useState(new Date());
+
   return (
       <main className={styles.main} >
-      <h1>calendar moment</h1>
+
+      <div className={styles.calendarContainer}>
+            <Calendar 
+                calendarType='gregory'
+                onChange={onChange}
+                value={value}
+            />
+        </div>
     </main>
   )
 }
