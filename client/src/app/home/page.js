f3dbcdd60277e0564347b66538e67703c5066289
Leo Weng <leoweng05@gmail.com>
2024-04-09 18:53:42 -0700

    Finished up day view
    - Added logos that change day by day, will accomodate for more logos added

--- client/src/app/home/page.js
+++ client/src/app/home/page.js
@@ -6,15 +6,25 @@ import Button from '@/components/button-component/button';
 import buttonStyle from '@/components/button-component/button.module.css';
 import TextInput from '@/components/submission-component/submission';
 import React from 'react';
+import Image from 'next/image'
 
 export default function Home() {
     const [inputValue, setInputValue] = useState('');
+
+    const logos = [
+        '/logos/logo1.png', 
+        '/logos/logo2.png', 
+    ];
 
     /* Real time date udpating */
     const currentDate = new Date();
     const day = currentDate.getDate();
     const dayOfWeek = currentDate.toLocaleString('default', { weekday: 'long' }).toLowerCase();
     const month = currentDate.toLocaleString('default', { month: 'long' }).toLowerCase();
+
+    /* Logo changes day by day */
+    const randomIndex = day % logos.length;
+    const logoUrl = logos[randomIndex];
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
@@ -24,7 +34,7 @@ export default function Home() {
         <main className={styles.main} >
             <h1 className={styles.date}>{dayOfWeek}, {month} {day}</h1>
             <div className={styles.circle}>
-
+                <Image width={128} height={128} src={logoUrl}></Image>
             </div>
             <h2 className={styles.message}>what are you grateful for today?</h2>
             <div className={styles.textInput}>
